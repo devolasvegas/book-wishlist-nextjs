ab790629c51f7a0c28df94c9c642db555d602f75
Devon Daviau <devondaviau@yahoo.ca>
2025-01-28 12:15:45 -0500

    add new function to get a single book, use it in the book details page

--- app/actions.ts
+++ app/actions.ts
@@ -43,3 +43,48 @@ export async function getBooks(): Promise<{
     message: null,
   };
 }
+
+export async function getBook(id: string): Promise<{
+  book: Book | null;
+  message: string | null;
+}> {
+  const query = {
+    query: `{
+      bookById(bookId: $id) {
+        id
+        title
+        author
+        genre
+        is_read
+      }
+    }`,
+    variables: {
+      id,
+    },
+  };
+
+  const response = await fetch(
+    process.env.SUPABASE_GRAPHQL_ENDPOINT as string,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        apikey: process.env.SUPABASE_ANON_KEY as string,
+      },
+      body: JSON.stringify(query),
+    }
+  ).then((res) => res.json());
+
+  if (!response.data) {
+    console.warn(
+      new Error(`Failed to fetch Book: ${response.errors[0]?.message}`)
+    );
+
+    return { book: null, message: "Failed to fetch Book" };
+  }
+
+  return {
+    book: response.data.bookById,
+    message: null,
+  };
+}
--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -1,8 +1,14 @@
+import { getBook } from "@/app/actions";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const id = (await params).id;
+  const { book, message } = await getBook(id);
+
+  console.log(book, message);
+
   return <div>My Book {id}</div>;
 }
