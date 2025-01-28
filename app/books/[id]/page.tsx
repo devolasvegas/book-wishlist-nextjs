84b87fd32fb35cc37258a6d5c0f8f7db00577145
Devon Daviau <devondaviau@yahoo.ca>
2025-01-28 14:31:54 -0500

    adjust width of book details container

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -20,7 +20,7 @@ export default async function Page({
 
   return (
     <main>
-      <div className="container mx-auto p-4">
+      <div className="max-w-7xl mx-auto p-4">
         <Suspense fallback={<BookDetailsSkeleton />}>
           <BookDetail book={book} />
         </Suspense>
