b8ee3706dcb8a4d0f47990bd3422c4ea90fa5d56
Devon Daviau <devondaviau@yahoo.ca>
2025-09-16 10:24:23 -0400

    add 'Edit' and 'Delete' buttons to component BookCard and update some text styles and layout therein

--- app/components/BookCard.tsx
+++ app/components/BookCard.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { type Book } from "../store/useBookStore";
+import { Button } from "@headlessui/react";
 
 interface BookCardProps {
   book: Book;
@@ -7,24 +8,46 @@ interface BookCardProps {
 
 const BookCard: React.FC<BookCardProps> = ({ book }) => {
   return (
-    <Link href={`/books/${book.id}`}>
-      <div className="p-4 border rounded shadow-sm">
-        <h2 className="text-lg font-semibold">{book.title}</h2>
-        <p className="text-sm text-gray-500">by {book.author}</p>
-        <p className="text-sm text-gray-600">Genre: {book.genre}</p>
-        <p className="mt-2">
-          <span
-            className={`px-2 py-1 text-xs rounded ${
-              book.is_read
-                ? "bg-green-100 text-green-800"
-                : "bg-yellow-100 text-yellow-800"
-            }`}
+    <div className="p-4 border rounded shadow-sm">
+      <div className="grid gap-6">
+        <div className="mb-4">
+          <h2
+            className="text-3xl font-semibold"
+            style={{ marginBottom: "0.5em" }}
           >
-            {book.is_read ? "Have Read" : "Want to Read"}
-          </span>
-        </p>
+            {book.title}
+          </h2>
+          <p className="text-xl text-gray-500">by {book.author}</p>
+          <p className="text-xl text-gray-600">Genre: {book.genre}</p>
+          <p className="mt-6">
+            <span
+              className={`px-2 py-1 text-lg rounded ${
+                book.is_read
+                  ? "bg-green-100 text-green-800"
+                  : "bg-yellow-100 text-yellow-800"
+              }`}
+            >
+              {book.is_read ? "Have Read" : "Want to Read"}
+            </span>
+          </p>
+        </div>
+        <div className="flex gap-2">
+          <Link
+            className="border rounded bg-blue-500 text-white text-lg"
+            style={{ padding: "0.75em 1em" }}
+            href={`/books/${book.id}`}
+          >
+            Edit Book Details
+          </Link>
+          <Button
+            className="border rounded bg-red-600 text-white text-lg"
+            style={{ padding: "0.75em 1em" }}
+          >
+            Delete Book
+          </Button>
+        </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
