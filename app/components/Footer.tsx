e0759dbca2c85fe0ac17e72741eba47d5fed4ba0
Devon Daviau <devondaviau@yahoo.ca>
2025-10-06 11:04:10 -0400

    adjust footer padding and margin

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,7 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="mt-8 mb-4 text-center text-sm text-gray-500">
-      <p>
-        &copy; {new Date().getFullYear()} Book Wishlist App. All rights
-        reserved.
-      </p>
+    <footer className="pb-10 text-center text-sm text-gray-500">
+      <p>&copy; {new Date().getFullYear()} Story Shelf. All rights reserved.</p>
     </footer>
   );
 };
