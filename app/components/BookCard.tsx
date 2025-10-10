23d8eb7818c719d7498564a91a10615b1a1f4b19
Devon Daviau <devondaviau@yahoo.ca>
2025-10-10 11:38:17 -0400

    re-engineer the details for the books and update book form to open in a modal from the bookcard component

--- app/components/BookCard.tsx
+++ app/components/BookCard.tsx
@@ -1,20 +1,26 @@
-import Link from "next/link";
+import { useState } from "react";
 import { Button } from "@headlessui/react";
 import { BookCheck, BookAlert } from "lucide-react";
 import { toast } from "react-toastify";
 
+import BookModal from "./BookModal";
+import BookDetail from "./BookDetail";
 import DeleteBookToast from "./DeleteBookToast";
 
 import { deleteBook } from "../actions";
 
 import { useBookStore } from "../providers/book-store-provider";
 import { type Book } from "../stores/book-store";
+import BookForm from "./BookForm";
 
 interface BookCardProps {
   book: Book;
 }
 
 const BookCard: React.FC<BookCardProps> = ({ book }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [bookDetail, setBookDetail] = useState<Book>(book);
+  const [viewMode, setViewMode] = useState<"view" | "edit">("view");
   const { deleteBook: deleteBookZustand } = useBookStore((state) => state);
 
   const handleDelete = () => {
@@ -32,10 +38,10 @@ const BookCard: React.FC<BookCardProps> = ({ book }) => {
 
   const handleToastClose = async (reason: string) => {
     if (reason === "delete") {
-      console.log("Deleting book with id:", book.id);
+      console.log("Deleting book with id:", bookDetail.id);
 
       // Returns { data: ... } or { errors: [ { message: string } ] }
-      const response = await deleteBook(book.id);
+      const response = await deleteBook(bookDetail.id);
 
       // Show our error(s) if we have them ...
       if (response.errors) {
@@ -55,7 +61,7 @@ const BookCard: React.FC<BookCardProps> = ({ book }) => {
       console.log("Delete response:", response);
 
       // Update Zustand store
-      deleteBookZustand(book.id);
+      deleteBookZustand(bookDetail.id);
 
       toast.success("Book deleted successfully!");
     } else if (reason === "cancel") {
@@ -65,48 +71,71 @@ const BookCard: React.FC<BookCardProps> = ({ book }) => {
     }
   };
 
+  // Close the modal and reset view mode to "view"
+  // so that when we open it again, we don't get the form
+  const handleModalClose = () => {
+    setIsOpen(false);
+    setViewMode("view");
+  };
+
   return (
-    <div className="pt-8 px-12 pb-6 border border-blueberrywhip rounded shadow-sm">
-      <div className="grid gap-6">
-        <div className="mb-4">
+    bookDetail && (
+      <div className="pt-8 px-12 pb-6 border border-blueberrywhip rounded shadow-sm">
+        <div className="grid gap-6">
           <div className="mb-4">
-            {book.is_read ? (
-              <div title="Have Read">
-                <BookCheck color="#00D390" size={"32px"} />
-              </div>
-            ) : (
-              <div title="Want to Read">
-                <BookAlert color="#E0A82E" size={"32px"} />
-              </div>
-            )}
+            <div className="mb-4">
+              {bookDetail.is_read ? (
+                <div title="Have Read">
+                  <BookCheck color="#00D390" size={"32px"} />
+                </div>
+              ) : (
+                <div title="Want to Read">
+                  <BookAlert color="#E0A82E" size={"32px"} />
+                </div>
+              )}
+            </div>
+            <h2
+              className="text-2xl font-semibold"
+              style={{ marginBottom: "0.5em" }}
+            >
+              {bookDetail.title}
+            </h2>
+            <p className="text-lg">Author {bookDetail.author}</p>
+            <p className="text-lg">Genre: {bookDetail.genre}</p>
           </div>
-          <h2
-            className="text-2xl font-semibold"
-            style={{ marginBottom: "0.5em" }}
-          >
-            {book.title}
-          </h2>
-          <p className="text-lg">Author {book.author}</p>
-          <p className="text-lg">Genre: {book.genre}</p>
-        </div>
-        <div className="flex gap-2">
-          <Link
-            className="border border-driedgoldenrod rounded bg-transparent text-driedgoldenrod text-lg hover:bg-driedgoldenrod hover:text-white transition-all duration-300"
-            style={{ padding: "0.75em 1em" }}
-            href={`/books/${book.id}`}
-          >
-            View Details
-          </Link>
-          <Button
-            className="border border-fusionred rounded bg-transparent text-fusionred text-lg hover:bg-fusionred hover:text-white transition-all duration-300"
-            style={{ padding: "0.75em 1em" }}
-            onClick={() => handleDelete && handleDelete()}
-          >
-            Delete Book
-          </Button>
+          <div className="flex gap-2">
+            <Button
+              className="border border-driedgoldenrod rounded bg-transparent text-driedgoldenrod text-lg hover:bg-driedgoldenrod hover:text-white transition-all duration-300"
+              style={{ padding: "0.75em 1em" }}
+              onClick={() => setIsOpen(true)}
+            >
+              View Details
+            </Button>
+            <Button
+              className="border border-fusionred rounded bg-transparent text-fusionred text-lg hover:bg-fusionred hover:text-white transition-all duration-300"
+              style={{ padding: "0.75em 1em" }}
+              onClick={() => handleDelete && handleDelete()}
+            >
+              Delete Book
+            </Button>
+            <BookModal isOpen={isOpen} onClose={handleModalClose}>
+              {viewMode === "view" ? (
+                <BookDetail
+                  book={bookDetail}
+                  viewModeHandler={() => setViewMode("edit")}
+                />
+              ) : (
+                <BookForm
+                  isUpdate={true}
+                  book={bookDetail}
+                  onUpdate={setBookDetail}
+                />
+              )}
+            </BookModal>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
--- app/components/BookDetail.tsx
+++ app/components/BookDetail.tsx
@@ -1,80 +1,49 @@
 "use client";
 
-import { use, useEffect, useState } from "react";
-import { notFound } from "next/navigation";
-
 import { Button } from "@headlessui/react";
 
-import EditBookModal from "./EditBookModal";
-
-import { useBookStore } from "../providers/book-store-provider";
-import { BookStore, type Book } from "../stores/book-store";
+import { type Book } from "../stores/book-store";
 
 const BookDetail = ({
-  id,
+  book,
+  viewModeHandler,
 }: {
-  id: Promise<{
-    id: string | null;
-  }>;
+  book?: Book;
+  viewModeHandler: () => void;
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [book, setBook] = useState<Book | undefined>(undefined);
-
-  const getBook = useBookStore((state: BookStore) => state.getBookById);
-
-  const { id: bookId } = use(id);
-
-  if (!bookId) {
-    notFound();
-  }
-
-  useEffect(() => {
-    if (bookId) {
-      setBook(getBook(bookId));
-    }
-  }, [bookId, setBook, getBook]);
-
   return (
     <>
       {book ? (
         <>
-          <div className="px-4 py-10 border rounded shadow-sm">
-            <div className="mb-9">
-              <h1 className="h1">{book.title}</h1>
-              <p className="text-gray-500">by {book.author}</p>
-              <p className="text-md text-gray-600">Genre: {book.genre}</p>
-              <p className="mt-2">
-                <span
-                  className={`px-2 py-1 text-s rounded ${
-                    book.is_read
-                      ? "bg-green-100 text-green-800"
-                      : "bg-yellow-100 text-yellow-800"
-                  }`}
-                >
-                  {book.is_read ? "Have Read" : "Want to Read"}
-                </span>
-              </p>
-            </div>
-            <div className="mb-9">
-              {book.description ? (
-                <p className="text-md text-gray-600">{book.description}</p>
-              ) : null}
-            </div>
-            <Button
-              className="border rounded bg-blue-500 text-white"
-              style={{ padding: "0.75em 1em" }}
-              onClick={() => setIsOpen(true)}
-              type="button"
-            >
-              Edit Book Details
-            </Button>
+          <div className="mb-9">
+            <h1 className="h1">{book.title}</h1>
+            <p className="text-gray-500">by {book.author}</p>
+            <p className="text-md text-gray-600">Genre: {book.genre}</p>
+            <p className="mt-2">
+              <span
+                className={`px-2 py-1 text-s rounded ${
+                  book.is_read
+                    ? "bg-green-100 text-green-800"
+                    : "bg-yellow-100 text-yellow-800"
+                }`}
+              >
+                {book.is_read ? "Have Read" : "Want to Read"}
+              </span>
+            </p>
           </div>
-          <EditBookModal
-            isOpen={isOpen}
-            onClose={() => setIsOpen(false)}
-            book={book}
-            onUpdate={setBook}
-          />
+          <div className="mb-9">
+            {book.description ? (
+              <p className="text-md text-gray-600">{book.description}</p>
+            ) : null}
+          </div>
+          <Button
+            className="border rounded bg-blue-500 text-white"
+            style={{ padding: "0.75em 1em" }}
+            onClick={viewModeHandler}
+            type="button"
+          >
+            Edit Book Details
+          </Button>
         </>
       ) : null}
       {/* {  ? <p>{message}</p> : null} */}
